20bba478f46e84df62943354a308887d1b6edb8a
Marco Calvo <e.marcocalvo@correo.ugr.es>
2025-05-09 10:26:47 +0200

    queda que se mueva bien el auxiliar por el mapa

--- Comportamientos_Jugador/auxiliar.cpp
+++ Comportamientos_Jugador/auxiliar.cpp
@@ -1245,8 +1245,8 @@ Action ComportamientoAuxiliar::ComportamientoAuxiliarNivel_3(Sensores sensores){
 }
 
 Action ComportamientoAuxiliar::ComportamientoAuxiliarNivel_4(Sensores sensores){
-
-	if(sensores.venpaca) return ComportamientoAuxiliarNivel_3(sensores);
+	if (sensores.energia >= 2750) return ComportamientoAuxiliarNivel_1(sensores);
+	else if(sensores.venpaca) return ComportamientoAuxiliarNivel_3(sensores);
 	else return IDLE;
 
 }
